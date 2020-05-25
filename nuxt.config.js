7f0dfb988e4c4766d0874d8efb1d888422439fd7
ayaka.kitabora <ayaka.kitabora@theport.jp>
2020-05-25 11:02:11 +0900

    init

---
+++ nuxt.config.js
@@ -0,0 +1,91 @@
+const StylelintPlugin = require('stylelint-webpack-plugin')
+module.exports = {
+  mode: 'spa',
+  env: {
+    apiKey: process.env.FIRE_APIKEY,
+    authDomain: process.env.FIRE_AUTH_DOMAIN,
+    databaseURL: process.env.FIRE_DATABASE_URL,
+    projectId: process.env.FIRE_PROJECT_ID,
+    storageBucket: process.env.FIRE_STORAGE_BUCKET,
+    messageSenderId: process.env.FIRE_MESSAGING_SENDER_ID,
+    appId: process.env.FIRE_APP_ID,
+    NODE_ENV: process.env.NODE_ENV
+  },
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['@/plugins/firebase'],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    '@nuxt/typescript-build',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    '@nuxtjs/stylelint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa'],
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    babel: {
+      presets({ isServer }) {
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
+    },
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue|ts)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+        config.plugins.push(
+          new StylelintPlugin({
+            files: ['**/*.vue', '**/*.scss']
+          })
+        )
+      }
+    }
+  }
+}
